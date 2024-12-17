bdffbb098244e211d054c9dbfac2ffab2715ee77
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-12-17 17:35:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-st.title("The BIG 10 Conference As It Should Be")
+st.title("Power 5 Conferences as they should be")
 
-st.write("Here are the original 10 teams of the BIG 10 Conference")
+st.write("Here are the rightfull teams of the each Power 5 Conference")
 data = pd.DataFrame({
-  'BIG 10 teams': ['Illinois', 'Indiana', 'Iowa', 'Michigan', 'Michigan State',
-                  'Minnesota', 'Northwestern', 'Ohio State', 'Purdue', 'Wisconsin']
+  'BIG 10 Conference': ['Illinois', 'Indiana', 'Iowa', 'Michigan', 'Michigan State',
+                  'Minnesota', 'Northwestern', 'Ohio State', 'Penn State', 'Purdue', 'Wisconsin'],
+  'BIG 12 Conference': ['Baylor', 'Colorado', 'Iowa State', 'Kansas', 'Kansas State', 'Missouri',
+                       'Nebraska', 'Oklahoma', 'Oklahoma State', 'Texas', 'Texas A&M', 'Texas Tech'],
+  'ACC Conference': ['Boston College', 'Clemson', 'Duke', 'Florida State', 'Georgia Tech', 'Maryland',
+                    'Miami', 'NC State', 'North Carolina', 'Virginia', 'Virginia Tech', 'Wake Forest'],
+  'PAC 10 Conference': ['Arizona', 'Arizona State', 'California', 'Oregon', 'Oregon State',
+                       'Stanford', 'UCLA', 'USC', 'Washington', 'Washington State'],
+  'SEC Conference': ['Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky',
+                    'LSU', 'Mississippi State', 'Ole Miss', 'South Carolina', 'Tennessee', 'Vanderbilt']
 })
 st.write(data)

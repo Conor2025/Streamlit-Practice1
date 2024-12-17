ab01edbe441526f88033f24e54f230246c04c59f
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-12-17 16:51:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import pandas as pd
 st.title("The BIG 10 Conference As It Should Be")
 
 st.write("Here are the original 10 teams of the BIG 10 Conference")
-st.write(pd.DataFrame({
+data = pd.DataFrame({
   'BIG 10 teams': ['Illinois', 'Indiana', 'Iowa', 'Michigan', 'Michigan State',
                   'Minnesota', 'Northwestern', 'Ohio State', 'Purdue', 'Wisconsin']
-}))
-st.dataframe(data)
+})
+st.write(data)
